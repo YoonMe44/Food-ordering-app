168a58c60197bb67d7333badb8c8478000404ebf
user <user@usernoMacBook-Pro-2.local>
2026-01-16 23:16:04 +0900

    add:cart page system

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,23 +1,34 @@
 import { Tabs } from "../../components/Tabs";
 import Button from "../../components/elements/Button";
 import { useSelector } from "react-redux";
-import { selectAllProducts } from "../../stores/menu/productsSlice";
+import { cartProducts } from "../../stores/cart/cartSlice";
 import { ReactComponent as ArrowRightSvg } from "../../assets/icons/arrow-right-long-svgrepo-com.svg";
+import useTabSwitch from "../../hooks/useTabSwitch";
 const Cart  = () =>{
-    const cart = useSelector(selectAllProducts);
-    const tabs = ['Summary', 'Details', 'Payment'];
+    const cart = useSelector(cartProducts);
+    const tabs = ['Summary', 'Delivery', 'Payment'];
+    const { currentTab, handleTabSwitch } = useTabSwitch(tabs, 'Summary');
+    console.log(cart);
     if(!cart || cart.length === 0){
-        console.log("Cart is empty");
         return(
-            <div className="bg-white text-black flex justify-center items-center p-4">
-                Your cart is empty
+            <div className="bg-white h-full text-black flex justify-center p-4">
+               <h1>Your cart is empty</h1>
             </div>
         )
     }
     return(
-    <>
-    Cart
-    </>
+        <div className="bg-white h-screen text-black mx-auto mt-2 border border-gray-200 p-4 md:w-2/3 rounded-lg shadow-md sm:p-6 lg:p-8">
+            <Tabs list={tabs} onTabSwitch={handleTabSwitch} activeTab={currentTab} />
+            <div className={`tabs ${currentTab !== 'Summary' ? 'hidden' : ''}`}>
+                Summary
+            </div>
+            <div className={`tabs ${currentTab !== 'Delivery' ? 'hidden' : ''}`}>
+                Address Form
+            </div>
+            <div className={`tabs ${currentTab !== 'Payment' ? 'hidden' : ''}`}>
+                Payment Form
+            </div>
+        </div>
     )
 }
 export default Cart;
